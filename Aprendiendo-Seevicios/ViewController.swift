c5c4b2caee179072e50e940e1fd10b7a2d758add
Carlos Mejia <carlosmejia083@gmail.com>
2020-01-21 10:30:02 -0500

    Clase 16 - Alamofire

--- Aprendiendo-Seevicios/ViewController.swift
+++ Aprendiendo-Seevicios/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Alamofire
 
 // 1. Crear modelo Codable (estructura) - OK
 // 2. Utilizar JSONDecoder para serializar Data a Modelo - OK
@@ -45,22 +46,21 @@ class ViewController: UIViewController {
         // Iniciamos el Loader
         activityIndicator.startAnimating()
         
-        URLSession.shared.dataTask(with: endpoint) { (data: Data?, _, error: Error?) in
-            
+        AF.request(endpoint, method: .get, parameters: nil).responseData { (response: AFDataResponse<Data>) in
             // Detener el loader
             DispatchQueue.main.async {
                 self.activityIndicator.stopAnimating()
             }
             
             
-            if error != nil {
+            if response.error != nil {
                 print("Hubo un error!")
                 
                 return
             }
             
             guard
-                let dataFromService = data,
+                let dataFromService = response.data,
                 let model: Human = try? JSONDecoder().decode(Human.self, from: dataFromService) else {
                     
                 return
@@ -71,9 +71,7 @@ class ViewController: UIViewController {
                 self.nameLabel.text = model.user
                 self.statusLabel.text = model.isHappy ? "Es feliz!" : "Es triste!"
             }
-            
-            
-        }.resume()
+        }
     }
 }
 
---
+++ Pods/Target Support Files/Pods-Aprendiendo-Seevicios/Pods-Aprendiendo-Seevicios-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Aprendiendo_Seevicios : NSObject
+@end
+@implementation PodsDummy_Pods_Aprendiendo_Seevicios
+@end
